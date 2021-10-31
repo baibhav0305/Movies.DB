0a8f48e15ff6fc1a093513019d300a4dc7083b46
Baibhav <baibhav030501@gmail.com>
2021-10-31 16:37:10 +0530

    fetch

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const MovieItem = () => {
-    return (
+const MovieItem = ({ selectedMovie }) => {
+  const [response, setResponse] = useState();
+  // tt0120338;
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const result = await axios(
+        `https://www.omdbapi.com/?i=${selectedMovie}&apikey=cb3c2a6`
+      );
+
+      console.log(result.data);
+      setResponse(result.data);
+    };
+    fetchDetails();
+  }, [selectedMovie]);
+
+  return (
+    <div>
+      {response && (
         <div>
-            {/* another api call here for big info */}
+          <h2>{response.Title}</h2>
+          <p>{response.Released}</p>
+          <p>{response.Genre}</p>
+          <p>{response.imdbRating}</p>
+          <p>{response.BoxOffice}</p>
+          <p>{response.Director}</p>
+          <p>{response.Actors}</p>
+          <p>{response.Plot}</p>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default MovieItem
+export default MovieItem;
